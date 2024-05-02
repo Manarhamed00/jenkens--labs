8f098ad8c6a2dc6ad4b1d55db9be0cffe517f04e
Manarhamed00 <manar.hamed762001@gmail.com>
2024-05-02 19:29:48 +0300

    edit jenkensfile for building app

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,20 @@
 pipeline {
     agent any
+    
     stages {
-        stage('Hello') {
+        stage('Checkout') {
             steps {
-                echo 'Hello, world! from manarrrrrr00'
+                git 'https://github.com/Manarhamed00/jenkens--labs'
+            }
+        }
+        stage('Install Dependencies') {
+            steps {
+                sh 'pip install -r requirements.txt'
+            }
+        }
+        stage('Run Tests') {
+            steps {
+                sh 'python manage.py test'
             }
         }
     }
-}
