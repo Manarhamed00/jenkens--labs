f1533c5fe0968360dea9be89712b3f995c33a624
Manarhamed00 <manar.hamed762001@gmail.com>
2024-05-03 00:22:56 +0300

    last

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -41,7 +41,8 @@ pipeline {
                      if (isUnix()) {
                 sh 'ssh -i home/newkey.pem ec2-user@3.87.252.140 "docker pull manarhamed00/my-django-app:latest && docker run -d manarhamed00/my-django-app:latest"'
             } else {
-                bat 'ssh -i C:\\Users\\smart\Downloads>\\newkey.pem ec2-user@3.87.252.140 "docker pull manarhamed00/my-django-app:latest && docker run -d manarhamed00/my-django-app:latest"'
+                bat 'ssh -i C:\\Users\\smart\\Downloads\\newkey.pem ec2-user@3.87.252.140 "docker pull manarhamed00/my-django-app:latest && docker run -d manarhamed00/my-django-app:latest"'
+
 
                     }
                 }
