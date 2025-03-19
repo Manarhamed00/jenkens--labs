ae1c7bc55a8b5285f70e1d290846864024189815
Manarhamed00 <123329277+Manarhamed00@users.noreply.github.com>
2025-03-19 02:42:53 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,33 @@
 pipeline {
     agent any
+
+    environment {
+        COMPOSE_FILE = 'docker-compose.yml'
+    }
+
     stages {
-        stage('Hello') {
+        stage('Clone Repository') {
             steps {
-                echo 'Hello, world! from manarrrrrr00'
+                git 'https://github.com/Manarhamed00/jenkens--labs.git'
             }
+        }
+
+        stage('Build and Run Containers') {
+            steps {
+                sh "docker-compose -f ${COMPOSE_FILE} up -d --build"
+            }
+        }
+
+        stage('Clean Up') {
+            steps {
+                sh "docker-compose down"
+            }
+        }
+    }
+
+    post {
+        always {
+            cleanWs()
         }
     }
 }
